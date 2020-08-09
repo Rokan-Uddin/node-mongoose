b3eb40cc5e7df87d7e7e01abddab43cd40820088
Rokan Uddin <rokanu55@gmail.com>
2020-08-09 20:31:55 +0600

    Mongoose Part 2

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
-
 const Dishes = require('./models/dishes');
-
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
@@ -9,27 +7,25 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
-        name: 'Rokan',
-        description: '1st test'
-    });
+    Dishes.create({
+        name: 'Rokan Uddin',
+        description: 'Test'
+    })
+    .then((dish) => {
+        console.log(dish);
+        
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 });
